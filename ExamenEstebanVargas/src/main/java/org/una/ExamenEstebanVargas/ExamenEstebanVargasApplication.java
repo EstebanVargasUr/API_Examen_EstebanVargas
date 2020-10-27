f7786dd446263929eb3ee932fbf5e226c4b0fecd
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-10-26 20:42:48 -0600

    Creación del proyecto

---
+++ ExamenEstebanVargas/src/main/java/org/una/ExamenEstebanVargas/ExamenEstebanVargasApplication.java
@@ -0,0 +1,13 @@
+package org.una.ExamenEstebanVargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenEstebanVargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenEstebanVargasApplication.class, args);
+	}
+
+}
